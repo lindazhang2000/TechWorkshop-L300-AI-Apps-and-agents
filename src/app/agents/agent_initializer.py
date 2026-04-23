4ed02dc9147b900c4a5741cdba9c75b18b268cc7
lindazhang2000 <lindazhang2000@gmail.com>
2026-04-23 10:19:11 -0400

    Add red teaming agent initializer

--- src/app/agents/agent_initializer.py
+++ src/app/agents/agent_initializer.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+az ad sp create-for-rbac --name "TechWorkshopL300AzureAI" --json-auth --role contributor --scopes /subscriptions/69f14792-2272-4f1d-96ff-44da16ea5f8a/resourceGroups/rg-techworkshop300from typing import List, Any
 from azure.ai.projects import AIProjectClient
 from azure.ai.projects.models import PromptAgentDefinition
 from dotenv import load_dotenv
